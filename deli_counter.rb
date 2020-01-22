0dc50e8e2fe5537b820b8368fa85b4b5890bf9af
Justin Pierce <jwpierce1984@gmail.com>
2020-01-22 17:26:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 
 def line (deli)
   if deli.empty?
-    return "The line is empty"
+    puts "the line is currently empty"
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
